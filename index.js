b42fdc53f619baf7510bc0f40434704fb7dd3968
koalament <koalament@gmail.com>
2020-05-16 22:14:24 +0430

    add nickname

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 let event = (process.env.GET_ALL_SITE_COMMENTS === "1" ? "site:" : "") + process.env.KEY + "_" + process.env.LAYER;
 
 nap_socket.on(new Buffer(event).toString("base64"), (comment) => {
-  const text = `${comment.text} <a href="https://whatsonchain.com/tx/${comment._txid}">🔗</a>\n\n${comment.key}`;
+  const text = `<b>${comment.nickname || "Unknown"}</b>\n${comment.text} <a href="https://whatsonchain.com/tx/${comment._txid}">🔗</a>\n\n${comment.key}`;
   bot.telegram.sendMessage(process.env.CHANNEL_NAME, text, { parse_mode: "HTML" }).catch(err => {
     console.log(err);
   })
